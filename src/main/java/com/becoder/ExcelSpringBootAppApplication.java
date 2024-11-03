1bc1f1fad9cb10ce90cd3ada81f277188de11f09
Pavy <daspabitra55@gmail.com>
2024-11-04 01:17:42 +0530

    intial commit

---
+++ src/main/java/com/becoder/ExcelSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelSpringBootAppApplication.class, args);
+	}
+
+}
